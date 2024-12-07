8c6177e6e1c31e6e35fe325a70ffcbb9f7ec2cd7
BorisKlco <klco@pm.me>
2024-12-07 12:16:46 -0500

    index view

--- 04-FileSharing/views/layout.php
+++ 04-FileSharing/views/layout.php
@@ -9,6 +9,18 @@
 </head>
 
 <body class="h-full">
+    <div class="bg-slate-300">
+        <nav class="mx-auto max-w-3xl flex justify-between py-2">
+            <div class="flex items-center gap-2">
+                <img width="48" height="48" src="https://img.icons8.com/clouds/100/share.png" class="drop-shadow" />
+                <h1 class="font-bold select-none">FileSharing</h1>
+            </div>
+            <div class="flex items-center gap-2">
+                <a href="#" class="px-4 py-1 border-2 rounded-md hover:bg-slate-200 hover:border-black">Login</a>
+                <a href="#" class="px-4 py-1 border-2 rounded-md hover:bg-slate-200 hover:border-black">Register</a>
+            </div>
+        </nav>
+    </div>
     <?php include $slot; ?>
 </body>
 
--- 04-FileSharing/views/main/index.php
+++ 04-FileSharing/views/main/index.php
@@ -1,3 +1,11 @@
-<div class="grid place-items-center h-full">
-    <p>Hi</p>
+<div class="bg-slate-200 w-full">
+    <div class="max-w-3xl mx-auto flex flex-col">
+
+        <div class="flex mt-10 py-4 gap-16 justify-center">
+            <div class="px-8 py-12 border rounded border-black">Select File</div>
+            <div class="px-8 py-12 border rounded border-black">Upload file</div>
+            <div class="px-8 py-12 border rounded border-black">Share a link</div>
+        </div>
+        <p>Hi</p>
+    </div>
 </div>
