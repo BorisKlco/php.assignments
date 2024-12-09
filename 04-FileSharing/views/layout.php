3465d06647ffc8cb299a2f9f0c6f9839bad4db20
BorisKlco <klco@pm.me>
2024-12-09 14:31:48 +0100

    navbar, fonts

--- 04-FileSharing/views/layout.php
+++ 04-FileSharing/views/layout.php
@@ -5,23 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Playwrite+HR+Lijeva:wght@100..400&family=Quicksand:wght@500&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
+<!-- https://knowledgehubtemplate.webflow.io/home-pages/home-v2 -->
 
-<body class="h-full">
-    <div>
-        <nav class="mx-auto max-w-3xl my-4 flex justify-between ring-1 shadow-sm ring-black/10 rounded-full py-2 px-4">
-            <div class="flex items-center gap-2">
+<body class="h-full font-['Nunito_Sans']">
+    <div class="absolute inset-x-0 sm:inset-x-80 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-96" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-violet-800 to-blue-400 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+    <div class="flex justify-center">
+        <nav class="max-w-3xl w-full my-4 mx-2 flex justify-between ring-1 shadow ring-black/10 rounded-full py-2 px-4 bg-slate-50">
+            <a href="#" class="flex justify-center items-center gap-2 hover:drop-shadow-[1px_1px_1px_rgba(70,200,150,0.5)]">
                 <img width="48" height="48" src="https://img.icons8.com/clouds/100/share.png" class="drop-shadow" />
-                <h1 class="font-bold select-none text-gray-800 text-lg">FileSharing</h1>
-            </div>
+                <h1 class="font-bold select-none text-zinc-700 text-lg font-['Playwrite_HR_Lijeva']">Dropie</h1>
+            </a>
             <div class="flex items-center gap-2">
-                <a href="#" class="px-4 py-1 text-gray-800 font-semibold text-sm">Register</a>
-                <a href="#" class="px-4 py-1 rounded-full bg-teal-400 ring-1 ring-sky-600 text-gray-800 font-semibold text-sm hover:bg-teal-500">Sign in</a>
+                <a href="#" class="px-4 py-1 text-gray-700 font-semibold text-sm hover:text-black hover:underline underline-offset-2">Register</a>
+                <a href="#" class="px-4 py-1 rounded-full bg-teal-400 ring-1 ring-black/10 text-gray-700 font-semibold text-sm hover:bg-teal-500 hover:text-white">Sign in</a>
             </div>
         </nav>
     </div>
     <?php include $slot; ?>
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-blue-600 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
 </body>
 
 </html>
--- 04-FileSharing/views/main/index.php
+++ 04-FileSharing/views/main/index.php
@@ -1,19 +1,19 @@
-<div class="bg-sky-100 w-full">
+<div class="mx-2">
     <div class="max-w-3xl mx-auto flex flex-col text-gray-800">
-        <h1 class="mt-10 text-center text-3xl font-semibold">Get Your Files Out There!</h1>
-        <p class="mt-2 text-center text-xl">Easily share your documents, photos, and others...</p>
-        <div class="flex mt-4 py-4 gap-12 justify-center mb-10">
-            <div class="w-44 h-40 border rounded-md flex items-center justify-center flex-col gap-4 ring-1 shadow-sm ring-sky-300">
-                <img width="64" height="64" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/58/external-select-camera-interface-1-sbts2018-flat-sbts2018.png" />
+        <h1 class="mt-4 sm:mt-10 text-center text-xl sm:text-3xl font-semibold">Get Your Files Out There!</h1>
+        <p class="mt-2 text-center sm:text-xl">Easily share your documents, photos, and others...</p>
+        <div class="flex mt-4 py-4 gap-6 sm:gap-12 justify-center mb-10">
+            <div class="w-44 h-20 sm:h-40 rounded-xl flex items-center justify-center flex-col gap-4 ring-1 shadow ring-black/5 bg-slate-50">
+                <img class="size-8 sm:size-16" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/58/external-select-camera-interface-1-sbts2018-flat-sbts2018.png" />
                 <p class="font-semibold  text-sm">Select File</p>
             </div>
-            <div class="w-44 h-40 border rounded flex items-center justify-center flex-col gap-4 ring-1 shadow-sm ring-sky-300">
-                <img width="64" height="64" src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-upload-project-planing-flatart-icons-flat-flatarticons.png" />
+            <div class="w-44 h-20 sm:h-40 rounded-xl flex items-center justify-center flex-col gap-4 ring-1 shadow ring-black/5 bg-slate-50">
+                <img class="size-8 sm:size-16" src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-upload-project-planing-flatart-icons-flat-flatarticons.png" />
                 <p class="font-semibold text-sm">Click upload</p>
             </div>
-            <div class="w-44 h-40 border rounded flex items-center justify-center flex-col gap-4 ring-1 shadow-sm ring-sky-300">
-                <img width="64" height="64" src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-email-contact-us-flatart-icons-flat-flatarticons-3.png" />
-                <p class="font-semibold  text-sm">Share a link</p>
+            <div class="w-44 h-20 sm:h-40 rounded-xl flex items-center justify-center flex-col gap-4 ring-1 shadow ring-black/5 bg-slate-50">
+                <img class="size-8 sm:size-16" src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-email-contact-us-flatart-icons-flat-flatarticons-3.png" />
+                <p class="font-semibold text-sm">Share a link</p>
             </div>
         </div>
 
